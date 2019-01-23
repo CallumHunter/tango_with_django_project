371c1fa9317536a679998ac3fbcd7c4f0e3b1157
Callum Hunter <2247415h@student.gla.ac.uk>
2019-01-23 15:27:36 +0000

    finished chp 3

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
     url(r'^admin/', admin.site.urls),
 ]
