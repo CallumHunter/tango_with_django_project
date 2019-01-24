af431aa75b810e5f93a230c32c439dfe3d27b2dc
Callum Hunter <2247415h@student.gla.ac.uk>
2019-01-24 14:33:34 +0000

    finished chp 6

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    #query for all categories currently stored
+    #order in descending order
+    #retrieve the top five
+    #place in context dictionary
+    #then pass to template engine
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
+    
     return render(request,'rango/index.html',context=context_dict)
 
 # Create your views here.
+
+from rango.models import Page
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+
+        pages = Page.objects.filter(category=category)
+
+        context_dict["pages"] = pages
+
+        context_dict["category"] = category
+    except Category.DoesNotExist:
+        context_dict["category"] = None
+        context_dict["category"] = None
+
+    return render(request, "rango/category.html",  context_dict)
+
+
+def about(request):
+    aboutString = "This is what this is all about"
+    context_dict = {"aboutString" : aboutString}
+    return render(request, "rango/about.html", context_dict)
